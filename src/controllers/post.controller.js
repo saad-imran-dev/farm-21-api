053dbe60de7b36ff008e4065677cecc207f04eb6
Saad Imran <saad.imran@contour-software.com>
2024-02-06 19:19:58 +0500

    Feature: Upload images with post and get images of post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -6,143 +6,173 @@ const { v4 } = require("uuid");
 const database = require("../data/Database");
 
 class postController {
-    static async createPost(req, res) {
-        console.info("--> Create Post");
+  static async createPost(req, res) {
+    console.info("--> Create Post");
 
-        try {
-            const { title, content, communityId } = req.body;
+    const transaction = await database.getTransaction();
 
-            await postValidation.post.validateAsync(req.body)
+    try {
+      const { title, content, communityId } = req.body;
 
-            await postRepo.createPost(title, content, req.uid, communityId)
+      await postValidation.post.validateAsync(req.body);
 
-            res.sendStatus(201);
-        } catch (error) {
-            if (error instanceof ValidationError) {
-                res.status(400).send("Invalid inputs");
-            } else {
-                res.sendStatus(500);
-            }
-            console.error(`Error: ${error.message}`);
-        }
+      const post = await postRepo.createPost(
+        title,
+        content,
+        req.uid,
+        communityId,
+        transaction
+      );
+
+      await Promise.all(
+        req.files.map(async (file) => {
+          const fileName = post.id + "/" + Date.now() + "-" + file.originalname;
+          await storage.uploadFile(fileName, file.buffer);
+          await postRepo.createAttachment(fileName, post.id, transaction);
+        })
+      );
+
+      await transaction.commit();
+      res.sendStatus(201);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+      await transaction.rollback();
+
+      if (error instanceof ValidationError) {
+        res.status(400).send("Invalid inputs");
+      } else {
+        res.sendStatus(500);
+      }
     }
+  }
 
-    static async getPosts(req, res) {
-        console.info("--> GET User Posts");
+  static async getPosts(req, res) {
+    console.info("--> GET User Posts");
 
-        try {
-            const posts = await postRepo.getPosts(req.uid);
+    try {
+      const posts = await postRepo.getPosts(req.uid);
 
-            res.status(200).send(posts);
-        } catch (error) {
-            res.sendStatus(500);
-            console.error(`Error: ${error.message}`);
-        }
+      res.status(200).send(posts);
+    } catch (error) {
+      res.sendStatus(500);
+      console.error(`Error: ${error.message}`);
     }
+  }
 
-    static async getPost(req, res) {
-        console.info("--> GET a Post");
+  static async getPost(req, res) {
+    console.info("--> GET a Post");
 
-        try {
-            const { id } = req.params
+    try {
+      const { id } = req.params;
 
-            const post = await postRepo.getPost(id);
+      const post = await postRepo.getPost(id);
 
-            if (!post) {
-                return res.sendStatus(404)
-            }
+      if (!post) {
+        return res.sendStatus(404);
+      }
 
-            const info = await postRepo.getInfo(id);
+      const attachments = await postRepo.getAttachments(id);
 
-            res.status(200).send({ ...post.dataValues, ...info });
-        } catch (error) {
-            res.sendStatus(500);
-            console.error(`Error: ${error.message}`);
-        }
+      const url = await Promise.all(
+        attachments.map(async (attachment) => {
+          const fileUrl = await storage.getUrl(attachment.fileName);
+          return fileUrl.publicUrl;
+        })
+      );
+
+      const info = await postRepo.getInfo(id);
+
+      res.status(200).send({ ...post.dataValues, ...info, attachments: url });
+    } catch (error) {
+      res.sendStatus(500);
+      console.error(`Error: ${error.message}`);
     }
+  }
 
-    static async deletePost(req, res) {
-        console.info("--> DELETE Post");
+  static async deletePost(req, res) {
+    console.info("--> DELETE Post");
+    const transaction = await database.getTransaction();
 
-        try {
-            const { id } = req.params
-            const transaction = database.getTransaction()
+    try {
+      const { id } = req.params;
 
-            const post = await postRepo.getPost(id)
+      const post = await postRepo.getPost(id);
 
-            if (!post) {
-                return res.sendStatus(404)
-            }
+      if (!post) {
+        return res.sendStatus(404);
+      }
 
-            if (post?.user_posts?.id !== req.uid) {
-                return res.sendStatus(401)
-            }
+      if (post?.user_posts?.id !== req.uid) {
+        return res.sendStatus(401);
+      }
 
-            const attachments = await postRepo.getAttachments(id)
+      const attachments = await postRepo.getAttachments(id);
 
-            if (attachments.length > 0) {
-                await Promise.all(attachments.forEach(async attachment => {
-                    await storage.deleteFile(attachment.fileName)
-                }));
-            }
+      if (attachments.length > 0) {
+        await Promise.all(
+          attachments.map(async (attachment) => {
+            await storage.deleteFile(attachment.fileName);
+          })
+        );
+      }
 
-            await postRepo.deleteAttachments(id)
+      await postRepo.deleteAttachments(id, transaction);
 
-            await postRepo.deletePost(id)
+      await postRepo.deletePost(id, transaction);
 
-            await transaction.commit()
+      await transaction.commit();
 
-            res.sendStatus(200);
-        } catch (error) {
-            await transaction.rollback()
-            res.sendStatus(500);
-            console.error(`Error: ${error.message}`);
-        }
+      res.sendStatus(200);
+    } catch (error) {
+      await transaction.rollback();
+      res.sendStatus(500);
+      console.error(`Error: ${error.message}`);
     }
+  }
 
-    static async likePost(req, res) {
-        console.info("--> Like a Post");
+  static async likePost(req, res) {
+    console.info("--> Like a Post");
 
-        try {
-            const { id } = req.params
+    try {
+      const { id } = req.params;
 
-            const post = await postRepo.getPost(id);
+      const post = await postRepo.getPost(id);
 
-            if (!post) {
-                return res.sendStatus(404)
-            }
+      if (!post) {
+        return res.sendStatus(404);
+      }
 
-            if (!(await postRepo.isPostLiked(id, req.uid))) {
-                await postRepo.likePost(id, req.uid);
-            }
+      if (!(await postRepo.isPostLiked(id, req.uid))) {
+        await postRepo.likePost(id, req.uid);
+      }
 
-            res.sendStatus(200);
-        } catch (error) {
-            res.sendStatus(500);
-            console.error(`Error: ${error.message}`);
-        }
+      res.sendStatus(200);
+    } catch (error) {
+      res.sendStatus(500);
+      console.error(`Error: ${error.message}`);
     }
+  }
 
-    static async dislikePost(req, res) {
-        console.info("--> Dislike a Post");
+  static async dislikePost(req, res) {
+    console.info("--> Dislike a Post");
 
-        try {
-            const { id } = req.params
+    try {
+      const { id } = req.params;
 
-            const post = await postRepo.getPost(id);
+      const post = await postRepo.getPost(id);
 
-            if (!post) {
-                return res.sendStatus(404)
-            }
+      if (!post) {
+        return res.sendStatus(404);
+      }
 
-            await postRepo.dislikePost(id, req.uid)
+      await postRepo.dislikePost(id, req.uid);
 
-            res.sendStatus(200);
-        } catch (error) {
-            res.sendStatus(500);
-            console.error(`Error: ${error.message}`);
-        }
+      res.sendStatus(200);
+    } catch (error) {
+      res.sendStatus(500);
+      console.error(`Error: ${error.message}`);
     }
+  }
 }
 
 module.exports = postController;
