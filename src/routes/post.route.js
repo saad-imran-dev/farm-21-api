9ef8fcbbf97326538da79d8aec738e0832e2ec47
Saad Imran <saad.imran@contour-software.com>
2024-02-15 16:10:21 +0500

    Documentation: Added swagger ui documentation for posts

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -69,7 +69,7 @@ module.exports = router
  * @swagger
  * /api/post:
  *   post:
- *     summary: Create a new post
+ *     summary: Create a new post. To add images, attach images files with feild name 'Attachments'
  *     tags: [Posts]
  *     security:
  *       - bearerAuth: []
@@ -86,6 +86,8 @@ module.exports = router
  *                 type: string
  *               communityId:
  *                 type: string
+ *               Attachments:
+ *                 type: file
  *     responses:
  *       201:
  *         description: Post created successfully
